c87ef4f777218876c599647d0eda6a38798c66cd
edan <molphone0328@gmail.com>
2025-09-16 15:42:49 +0900

    [Feat] 시네마신 카메라 기능 업데이트, 달리기 기능 추가

--- GE_Class_2nd/Assets/Scripts/PlayerController.cs
+++ GE_Class_2nd/Assets/Scripts/PlayerController.cs
@@ -1,41 +1,98 @@
+using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 5f;
-    public float jumpPower = 5f;
+    private float currentSpeed;
+    private float stopSpeed = 0f;
+    private float walkSpeed = 5f;
+    private float runSpeed = 12f;
+    private float jumpPower = 5f;
     public float gravity = -9.81f;
+
+    public CinemachineVirtualCamera virtualCam;
+    public float rotationSpeed = 10f;
+    private CinemachinePOV pov;
+
     private CharacterController controller;
     private Vector3 velocity;
     public bool isGrounded;
+    public bool isRunning;
+
+    public CinemachineSwitcher cinemachineSwitcher;
 
     void Start()
     {
-        controller = GetComponent<CharacterController>();    
+        controller = GetComponent<CharacterController>();
+        pov = virtualCam.GetCinemachineComponent<CinemachinePOV>();
     }
 
     void Update()
     {
+        //if (Cursor.lockState != CursorLockMode.Locked) return;
+
+        //if (cinemachineSwitcher.usingFreeLook == true) return;
+
+        if (cinemachineSwitcher.usingFreeLook == false)
+        {
+            if (Input.GetKey(KeyCode.LeftShift))                     //왼쪽 쉬프트를 눌러서 달리기로 변경
+            {
+                currentSpeed = runSpeed;
+                isRunning = true;
+            }
+            else
+            {
+                currentSpeed = walkSpeed;
+                isRunning = false;
+            }
+        }
+
+        if (isRunning)
+        {
+            virtualCam.m_Lens.FieldOfView = Mathf.Lerp(virtualCam.m_Lens.FieldOfView, 65f, Time.deltaTime * 5f);
+        }
+        else
+        {
+            virtualCam.m_Lens.FieldOfView = Mathf.Lerp(virtualCam.m_Lens.FieldOfView, 40f, Time.deltaTime * 5f);
+        }
+
         isGrounded = controller.isGrounded;
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
         // 카메라 기준 방향 계산
-        Transform cam = Camera.main.transform;
-
-        Vector3 camForward = cam.forward;
-        Vector3 camRight = cam.right;
-
-        // y축 방향 제거 (수평 이동만)
-        camForward.y = 0f;
-        camRight.y = 0f;
+        Vector3 camForward = virtualCam.transform.forward;
+        camForward.y = 0;
         camForward.Normalize();
+
+        Vector3 camRight = virtualCam.transform.right;
+        camRight.y = 0;
         camRight.Normalize();
 
-        Vector3 move = camRight * x + camForward * z;
-        controller.Move(move * speed * Time.deltaTime);
+        Vector3 move = (camForward * z + camRight * x).normalized;  //이동 방향 = 카메라 forward/right기반
+        controller.Move(move * currentSpeed * Time.deltaTime);
+
+        if (cinemachineSwitcher.usingFreeLook == true)
+        {
+            currentSpeed = stopSpeed;
+            jumpPower = 0f;
+        }
+        else
+        {
+            currentSpeed = walkSpeed;
+            jumpPower = 5f;
+        }
+
+            float cameraYaw = pov.m_HorizontalAxis.Value;   //마우스 좌우 회전값
+        Quaternion targetRot = Quaternion.Euler(0f, cameraYaw, 0f);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRot, rotationSpeed * Time.deltaTime);
 
         //여기부터 점프
         if (isGrounded && Input.GetKeyDown(KeyCode.Space))
@@ -47,22 +104,4 @@ public class PlayerController : MonoBehaviour
         controller.Move(velocity * Time.deltaTime);
     }
 
-
-    //교수님꺼
-    //void Update()
-    //{
-    //    isGrounded = controller.isGrounded;
-    //    float x = Input.GetAxis("Horizontal");
-    //    float z = Input.GetAxis("Vertical");
-
-    //    Vector3 move = new Vector3(x, 0, z);
-    //    controller.Move(move * speed * Time.deltaTime);
-
-    //    if (isGrounded && Input.GetKeyDown(KeyCode.Space))
-    //    {
-    //        velocity.y = jumpPower;
-    //    }
-    //    velocity.y += gravity * Time.deltaTime;
-    //    controller.Move(velocity * Time.deltaTime);
-    //}
 }
